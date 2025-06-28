d298fb61bbb7f00b9cd4e08f8e623e67435ac931
issath-voisek <issath@voisek.com>
2025-06-28 23:21:43 +0530

    Added RBAC: Role-Based Access Control

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import CustomTokenObtainPairView
+
+from accounts.views import AdminOnlyView, CustomTokenObtainPairView, LogoutView
+
 from rest_framework_simplejwt.views import TokenRefreshView
+
 
 urlpatterns = [
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+    path('admin-only/', AdminOnlyView.as_view(), name='admin-only'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -8,6 +8,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.token_blacklist.models import BlacklistedToken, OutstandingToken
 
+from accounts.permissions import IsAdmin
+
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
@@ -29,7 +31,15 @@ class LogoutView(APIView):
             refresh_token = request.data.get("refresh")
             token = RefreshToken(refresh_token)
             token.blacklist()
+
             return Response(status=status.HTTP_205_RESET_CONTENT)
         
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        
+
+class AdminOnlyView(APIView):
+    permission_classes = [IsAuthenticated, IsAdmin]
+
+    def get(self, request):
+        return Response({"message": "Hello, Admin!"})
