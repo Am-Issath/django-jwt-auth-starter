ebe65e40d1502bdf004c6d004ac11f28a244d079
issath-voisek <issath@voisek.com>
2025-06-28 22:53:23 +0530

    Created accounts view with CustomTokenObtainPairSerializer

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-# Create your views here.
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        data['username'] = self.user.username
+        data['role'] = self.user.role
+        return data
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
